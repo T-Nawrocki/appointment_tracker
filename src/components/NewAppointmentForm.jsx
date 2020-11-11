4b28c1c53339d525cb60b32341a4983076b64ec4
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-11 10:27:13 +0000

    basic form elements added, tests pass

--- src/components/NewAppointmentForm.jsx
+++ src/components/NewAppointmentForm.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 
 const NewAppointmentForm = (props) => {
+  
   return ( 
     <form id="new-appointment-form">
-      
+      <label htmlFor="new-appointment-title">Title</label>
+      <input type="text"
+             name="new-appointment-title" 
+             id="new-appointment-title" />
+
+      <label htmlFor="new-appointment-date">Date</label>
+      <input type="date" 
+             name="new-appointment-date" 
+             id="new-appointment-date"/>
+
+      <label htmlFor="new-appointment-time">Time</label>
+      <select name="new-appointment-time" id="new-appointment-time">
+        
+      </select>       
+
+      <input name="new-appointment-submit" 
+             type="submit" 
+             value="Book appointment" />
     </form>
    );
 }
