d6ceb2bfa2b9cd656a8c75e7b553bdd9507d4a7d
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-11 13:53:39 +0000

    form inputs linked to state

--- src/components/NewAppointmentForm.jsx
+++ src/components/NewAppointmentForm.jsx
@@ -18,8 +18,20 @@ export const availableTimes = () => {
 const NewAppointmentForm = (props) => {
   
   const [title, setTitle] = useState("");
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState("");
   const [time, setTime] = useState("");
+
+  const handleTitleChange = event => {
+    setTitle(event.target.value);
+  }
+
+  const handleDateChange = event => {
+    setDate(event.target.value);
+  }
+
+  const handleTimeChange = event => {
+    setTime(event.target.value);
+  }
   
   return ( 
     <form id="new-appointment-form">
@@ -27,20 +39,27 @@ const NewAppointmentForm = (props) => {
       <input type="text"
              name="new-appointment-title" 
              id="new-appointment-title"
-             value={title} />
+             value={title}
+             onChange={handleTitleChange} />
 
       <label htmlFor="new-appointment-date">Date</label>
       <input type="date" 
              name="new-appointment-date" 
              id="new-appointment-date"
-             value={date} />
+             value={date}
+             onChange = {handleDateChange} />
 
       <label htmlFor="new-appointment-time">Time</label>
-      <select name="new-appointment-time" id="new-appointment-time" value={time}>
+      <select name="new-appointment-time" 
+              id="new-appointment-time" 
+              value={time}
+              onChange={handleTimeChange}>
+
         <option disabled value="">--:--</option>
         {availableTimes().map((availableTime, index) => {
           return <option key={index} value={availableTime}>{availableTime}</option>
         })}
+
       </select>       
 
       <input name="new-appointment-submit" 
