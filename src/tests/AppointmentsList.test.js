a227d6bcb5cfbf45154afe4398e0271117515629
Tomek Nawrocki <tnawrocki1801@gmail.com>
2020-11-12 12:25:23 +0000

    render test for appointments list added

---
+++ src/tests/AppointmentsList.test.js
@@ -0,0 +1,14 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
+
+import AppointmentsList from '../components/AppointmentsList';
+
+configure({ adapter: new Adapter() });
+
+describe('Appointments List', () => {
+  it('should render the list', () => {
+    const tree = renderer.create(<AppointmentsList />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
